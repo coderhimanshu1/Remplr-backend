d24e2fbf4ae92d1b3b327a6033c2a1b9db752099
himanshukumar30 <himanshuk.kumar01@gmail.com>
2023-07-26 11:19:33 -0700

    adding ingredient_nutrients table

--- remplr-schema.sql
+++ remplr-schema.sql
@@ -1,29 +1,6 @@
--- users table
-CREATE TABLE IF NOT EXISTS users (
-    id INT PRIMARY KEY,
-    username VARCHAR(255),
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
-    email VARCHAR(255),
-    is_admin BOOLEAN,
-    is_client BOOLEAN,
-    is_nutritionist BOOLEAN
-);
-
--- ingredients table
-CREATE TABLE IF NOT EXISTS ingredients (
-    id INT PRIMARY KEY,
-    aisle VARCHAR(255),
-    image VARCHAR(255),
-    name VARCHAR(255),
-    original TEXT,
-    amount DECIMAL,
-    unit VARCHAR(50)
-);
-
 -- recipes table
 CREATE TABLE IF NOT EXISTS recipes (
-    id INT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     vegetarian BOOLEAN,
     vegan BOOLEAN,
     dairyfree BOOLEAN,
@@ -40,60 +17,103 @@ CREATE TABLE IF NOT EXISTS recipes (
     summary TEXT
 );
 
+-- users table
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255),
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    email VARCHAR(255),
+    is_admin BOOLEAN,
+    is_client BOOLEAN,
+    is_nutritionist BOOLEAN
+);
+
+-- ingredients table
+CREATE TABLE IF NOT EXISTS ingredients (
+    id SERIAL PRIMARY KEY,
+    aisle VARCHAR(255),
+    image VARCHAR(255),
+    name VARCHAR(255),
+    original TEXT,
+    amount DECIMAL,
+    unit VARCHAR(50)
+);
+
+-- ingredient nutrients table
+
+    CREATE TABLE IF NOT EXISTS ingredient_nutrients (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255),
+        amount NUMERIC,
+        unit VARCHAR(255),
+        percentofdailyneeds NUMERIC,
+        ingredient_id INT,
+        FOREIGN KEY (ingredient_id) REFERENCES ingredients(id)
+        ON UPDATE CASCADE
+    );
+
 --instructions table
 CREATE TABLE IF NOT EXISTS instructions (
-    id INT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     recipe_id INT,
     number INT,
     step TEXT,
     FOREIGN KEY (recipe_id) REFERENCES recipes(id)
+    ON UPDATE CASCADE
 );
 
--- nutrients table
-CREATE TABLE IF NOT EXISTS nutrients (
-    id INT PRIMARY KEY,
+-- recipe nutrients table
+CREATE TABLE IF NOT EXISTS recipe_nutrients (
+    id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     amount DECIMAL,
     unit VARCHAR(50),
     percentofdailyneeds DECIMAL,
     recipe_id INT,
     FOREIGN KEY (recipe_id) REFERENCES recipes(id)
+    ON UPDATE CASCADE
 );
 
 -- recipetoingredients table
 CREATE TABLE IF NOT EXISTS recipe_ingredients (
-    id INT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     recipe_id INT,
     ingredient_id INT,
     FOREIGN KEY (recipe_id) REFERENCES recipes(id),
     FOREIGN KEY (ingredient_id) REFERENCES ingredients(id)
+    ON UPDATE CASCADE
 );
 
 
 -- meal_plans table
 CREATE TABLE IF NOT EXISTS meal_plans (
-    id INT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     created_by INT,
     FOREIGN KEY (created_by) REFERENCES users(id)
+    ON UPDATE CASCADE
 );
 
 -- meal_plan_recipes table
 CREATE TABLE IF NOT EXISTS meal_plan_recipes (
-    id INT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     meal_plan_id INT,
     recipe_id INT,
     meal_type VARCHAR(255),
     meal_day INT,
     FOREIGN KEY (meal_plan_id) REFERENCES meal_plans(id),
     FOREIGN KEY (recipe_id) REFERENCES recipes(id)
+    ON UPDATE CASCADE
 );
 
 -- meal_plan_users table
 CREATE TABLE IF NOT EXISTS meal_plan_users (
-    id INT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     user_id INT,
     meal_plan_id INT,
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (meal_plan_id) REFERENCES meal_plans(id)
+    ON UPDATE CASCADE
 );
+
