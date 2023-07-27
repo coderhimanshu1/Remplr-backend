369b49826b8139948091de1096b8a3a32cbb9493
himanshukumar30 <himanshuk.kumar01@gmail.com>
2023-07-26 23:34:21 -0700

    adding tokens helper function

---
+++ helpers/tokens.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../config");
+
+/** return signed JWT from user data. */
+
+function createToken(user) {
+  console.assert(
+    user.isAdmin !== undefined,
+    user.isNutritionist !== undefined,
+    user.isClient !== undefined,
+    "createToken passed user without isAdmin, isNutritionist and isClient property"
+  );
+
+  let payload = {
+    username: user.username,
+    isAdmin: user.isAdmin || false,
+    isAdmin: user.isNutritionist || false,
+    isAdmin: user.isClient || false,
+  };
+
+  return jwt.sign(payload, SECRET_KEY);
+}
+
+module.exports = { createToken };
