f3fa516e8ee3be64a46c227dc381589520166c8d
himanshukumar30 <himanshuk.kumar01@gmail.com>
2023-07-31 18:25:38 -0700

    adding authorization for mealplans routes

--- routes/mealplans.js
+++ routes/mealplans.js
@@ -4,6 +4,14 @@ const jsonschema = require("jsonschema");
 const MealPlan = require("../models/mealplan");
 const express = require("express");
 const { BadRequestError } = require("../expressError");
+const {
+  ensureLoggedIn,
+  ensureCorrectUserOrAdmin,
+  ensureAdminOrNutritionist,
+  ensureAdmin,
+  ensureNutritionist,
+  ensureClient,
+} = require("../middleware/auth");
 
 const router = express.Router({ mergeParams: true });
 
@@ -13,9 +21,9 @@ const router = express.Router({ mergeParams: true });
  *
  * Returns { id, name, created_by, recipes : [{recipe_id, meal_type, meal_day},...] }
  *
- * TODO: Authorization required
+ * Authorization required: admin or nutritionist
  */
-router.post("/", async (req, res, next) => {
+router.post("/", ensureAdminOrNutritionist, async (req, res, next) => {
   try {
     // Create the meal plan
     const mealPlan = await MealPlan.create({
@@ -42,10 +50,10 @@ router.post("/", async (req, res, next) => {
 /** GET /  =>
  *   { mealPlans: [ { id, name, created_by }, ...] }
  *
- * TODO: Authorization required
+ * Authorization required: admin or nutritionist
  */
 
-router.get("/", async (req, res, next) => {
+router.get("/", ensureAdminOrNutritionist, async (req, res, next) => {
   try {
     const mealPlans = await MealPlan.findAll();
     return res.json({ mealPlans });
@@ -58,16 +66,20 @@ router.get("/", async (req, res, next) => {
  *  { id, name, created_by, recipes: [{ { id, vegetarian, vegan, dairyfree, weightwatchersmartpoints, creditstext,
  *  title, readyinminutes, servings, sourceurl, image, imagetype, dishtype, diets, summary } ,meal_type, meal_day}, ...] }
  *
- * TODO: Authorization required
+ *  Authorization required: admin or nutritionist
  */
-router.get("/:mealPlanId", async (req, res, next) => {
-  try {
-    const mealPlan = await MealPlan.get(req.params.mealPlanId);
-    return res.json({ mealPlan });
-  } catch (err) {
-    return next(err);
+router.get(
+  "/:mealPlanId",
+  ensureAdminOrNutritionist,
+  async (req, res, next) => {
+    try {
+      const mealPlan = await MealPlan.get(req.params.mealPlanId);
+      return res.json({ mealPlan });
+    } catch (err) {
+      return next(err);
+    }
   }
-});
+);
 
 /** PATCH /[mealPlanId] { mealPlan } => { mealPlan }
  *
@@ -75,17 +87,21 @@ router.get("/:mealPlanId", async (req, res, next) => {
  *
  * Returns { id, name, created_by }
  *
- * TODO: Authorization required
+ * Authorization required: admin or nutritionist
  */
 
-router.patch("/:mealPlanId", async function (req, res, next) {
-  try {
-    const mealPlan = await MealPlan.update(req.params.mealPlanId, req.body);
-    return res.json({ mealPlan });
-  } catch (err) {
-    return next(err);
+router.patch(
+  "/:mealPlanId",
+  ensureAdminOrNutritionist,
+  async function (req, res, next) {
+    try {
+      const mealPlan = await MealPlan.update(req.params.mealPlanId, req.body);
+      return res.json({ mealPlan });
+    } catch (err) {
+      return next(err);
+    }
   }
-});
+);
 
 /**
  * POST /[mealPlanId]/recipes { recipe } => { recipe }
@@ -96,31 +112,36 @@ router.patch("/:mealPlanId", async function (req, res, next) {
  *
  * This route is used to add a new recipe to the specified meal plan.
  *
- * TODO: Authorization required
+ * Authorization required: admin or nutritionist
  */
 
-router.post("/:mealPlanId/recipes", async function (req, res, next) {
-  try {
-    const recipe = await MealPlan.addRecipeToMealPlan({
-      meal_plan_id: req.params.mealPlanId,
-      ...req.body,
-    });
-    return res.status(201).json({ recipe });
-  } catch (err) {
-    return next(err);
+router.post(
+  "/:mealPlanId/recipes",
+  ensureAdminOrNutritionist,
+  async function (req, res, next) {
+    try {
+      const recipe = await MealPlan.addRecipeToMealPlan({
+        meal_plan_id: req.params.mealPlanId,
+        ...req.body,
+      });
+      return res.status(201).json({ recipe });
+    } catch (err) {
+      return next(err);
+    }
   }
-});
+);
 
 /**
  * DELETE /[mealPlanId]/recipes/[recipeId] { recipe } => { deleted: recipeId }
  *
  * This route is used to remove a recipe from the specified meal plan.
  *
- * TODO: Authorization required
+ * Authorization required: admin or nutritionist
  */
 
 router.delete(
   "/:mealPlanId/recipes/:recipeId",
+  ensureAdminOrNutritionist,
   async function (req, res, next) {
     try {
       await MealPlan.removeRecipe(req.params.recipeId);
@@ -136,10 +157,10 @@ router.delete(
  *
  * This route is used to delete the specified meal plan.
  *
- * TODO: Authorization required
+ * Authorization required: admin
  */
 
-router.delete("/:mealPlanId", async function (req, res, next) {
+router.delete("/:mealPlanId", ensureAdmin, async function (req, res, next) {
   try {
     await MealPlan.remove(req.params.mealPlanId);
     return res.json({ "deleted meal plan": req.params.mealPlanId });
